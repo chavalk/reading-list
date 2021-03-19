2a906c069fcfcab76f05ed08ad9efbc76a44dcc0
chavalk <chavalk@hotmail.com>
2021-03-19 17:09:08 -0500

    Deploying

---
+++ client/build/precache-manifest.b050b472e04f040a05e237058501dd43.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7d799eb175a0d2fa492a78ee3709bb1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "37d5fd3911209b4c1599",
+    "url": "/static/css/main.997d4878.chunk.css"
+  },
+  {
+    "revision": "dcd954fd859b7c60d919",
+    "url": "/static/js/2.e265699e.chunk.js"
+  },
+  {
+    "revision": "223be75f6809993b56a94be00d710bed",
+    "url": "/static/js/2.e265699e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "37d5fd3911209b4c1599",
+    "url": "/static/js/main.6a2aa5ab.chunk.js"
+  },
+  {
+    "revision": "d08766e1dd2bf4f6c209",
+    "url": "/static/js/runtime-main.992eac29.js"
+  }
+]);
